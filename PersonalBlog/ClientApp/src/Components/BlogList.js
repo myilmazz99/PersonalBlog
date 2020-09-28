626595dc2b6cc097b600ff451a074b341dc91da6
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-09-28 23:36:35 +0300

    fixed effect loop

--- PersonalBlog/ClientApp/src/Components/BlogList.js
+++ PersonalBlog/ClientApp/src/Components/BlogList.js
@@ -9,108 +9,111 @@ import CategoryList from "./CategoryList";
 import LoadingSpinner from "./Utilities/LoadingSpinner";
 
 const BlogList = ({
-    path,
-    blogReducer: { blogs, loadingBlogs, page, hasMoreBlogs, blogCount },
-    getBlogs,
-    currentCategory,
+  path,
+  blogReducer: { blogs, loadingBlogs, page, hasMoreBlogs, blogCount },
+  getBlogs,
+  currentCategory,
 }) => {
-    const [onBlogsPage] = useState(path.indexOf("blogs") > 0);
-    const observer = useRef();
+  const [onBlogsPage] = useState(path.indexOf("blogs") > 0);
+  const observer = useRef();
 
-    const lastBlogRef = (node) => {
-        if (loadingBlogs) return;
-        if (observer.current) observer.current.disconnect();
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting && hasMoreBlogs && onBlogsPage) {
-                getBlogs(page);
-            }
-        })
+  const lastBlogRef = (node) => {
+    if (loadingBlogs) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMoreBlogs && onBlogsPage) {
+        getBlogs(page);
+      }
+    });
 
-        if (node) observer.current.observe(node);
+    if (node) observer.current.observe(node);
+  };
+
+  useEffect(() => {
+    if (blogs.length < blogCount) {
+      getBlogs(page);
     }
+  }, [blogCount]);
 
-    useEffect(() => {
-        if (blogs.length === 0 || blogs.length < blogCount) {
-            getBlogs(page);
-        }
-    }, []);
+  if (!onBlogsPage) {
+    blogs = blogs.slice(0, 3);
+  }
 
-    if (!onBlogsPage) {
-        blogs = blogs.slice(0, 3);
-    }
+  return (
+    <>
+      <main className="container">
+        {onBlogsPage ? (
+          <div className="blog-header">
+            <CategoryList />
+          </div>
+        ) : (
+          <div className="blog-header">Son Eklenenler</div>
+        )}
 
-    return (
-        <>
-            <main className="container">
-                {onBlogsPage ? (
-                    <div className="blog-header">
-                        <CategoryList />
-                    </div>
-                ) : (
-                        <div className="blog-header">Son Eklenenler</div>
-                    )}
+        {blogs &&
+          blogs
+            .filter((blog) =>
+              currentCategory && onBlogsPage
+                ? blog.categoryName === currentCategory
+                : blog
+            )
+            .map((blog, i) => (
+              <Link
+                ref={blogs.length === i + 1 ? lastBlogRef : null}
+                key={blog.blogId}
+                to={`/blogs/${blog.blogId}`}
+              >
+                <div className="blog-preview">
+                  <div className="blog-img">
+                    <img src={blog.mainImage} alt="" />
+                  </div>
 
-                {blogs &&
-                    blogs
-                        .filter((blog) =>
-                            currentCategory && onBlogsPage
-                                ? blog.categoryName === currentCategory
-                                : blog
-                        )
-                        .map((blog, i) => (
-                            <Link ref={blogs.length === i + 1 ? lastBlogRef : null} key={blog.blogId} to={`/blogs/${blog.blogId}`}>
-                                <div className="blog-preview">
-                                    <div className="blog-img">
-                                        <img src={blog.mainImage} alt="" />
-                                    </div>
+                  <div className="blog-details">
+                    <h2>{blog.header}</h2>
+                    <div className="category">{blog.categoryName}</div>
+                    <small>
+                      <span>
+                        {blog.addedDate}, by {blog.writerName}
+                      </span>
+                      <span>
+                        <span className="blog-details-icon">
+                          <i className="fas fa-eye"></i> {blog.viewCount}
+                        </span>
+                        <span className="blog-details-icon">
+                          <i className="fas fa-comment"></i> {blog.commentCount}
+                        </span>
+                      </span>
+                    </small>
+                  </div>
+                </div>
+              </Link>
+            ))}
 
-                                    <div className="blog-details">
-                                        <h2>{blog.header}</h2>
-                                        <div className="category">{blog.categoryName}</div>
-                                        <small>
-                                            <span>
-                                                {blog.addedDate}, by {blog.writerName}
-                                            </span>
-                                            <span>
-                                                <span className="blog-details-icon">
-                                                    <i className="fas fa-eye"></i> {blog.viewCount}
-                                                </span>
-                                                <span className="blog-details-icon">
-                                                    <i className="fas fa-comment"></i> {blog.commentCount}
-                                                </span>
-                                            </span>
-                                        </small>
-                                    </div>
-                                </div>
-                            </Link>
-                        ))
-                }
+        {loadingBlogs ? <LoadingSpinner /> : ""}
 
-                {loadingBlogs ? <LoadingSpinner/> : ""}
-
-                {onBlogsPage ? (
-                    ""
-                ) : (
-                        <div className="link-to-blogs">
-                            <Link to="/blogs">Tüm Yazılarım</Link>
-                        </div>
-                    )}
-            </main>
-        </>
-    );
+        {onBlogsPage ? (
+          ""
+        ) : (
+          <div className="link-to-blogs">
+            <Link to="/blogs">Tüm Yazılarım</Link>
+          </div>
+        )}
+      </main>
+    </>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        blogReducer: state.blogReducer,
-        currentCategory: state.categoryReducer.currentCategory.categoryName,
-    };
+  return {
+    blogReducer: state.blogReducer,
+    currentCategory: state.categoryReducer.currentCategory.categoryName,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getBlogs: bindActionCreators(getBlogs, dispatch),
-    };
+  return {
+    getBlogs: bindActionCreators(getBlogs, dispatch),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BlogList);

9c4bbadf7214c926973b85b4459007c623a76fd3
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-10-03 02:21:06 +0300

    typo fix

--- PersonalBlog/ClientApp/src/Pages/NotFound.js
+++ PersonalBlog/ClientApp/src/Pages/NotFound.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import Code404 from '../img/Code404.svg';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import Code404 from "../img/Code404.svg";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const NotFound = () => {
+  const section = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
 
-    const section = {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height:"100vh"
-    }
+  const img = {
+    display: "block",
+    width: "50%",
+    maxWidth: "500px",
+  };
 
-    const img = {
-        display: "block",
-        width: "50%",
-        maxWidth:"500px",
-    }
+  const p = {
+    marginTop: "2rem",
+  };
 
-    const p = {
-        marginTop:"2rem",
-    }
+  const a = {
+    color: "red",
+  };
 
-    const a = {
-        color:"red"
-    }
-
-    return (
-        <section style={section}>
-            <Helmet>
-                <title>Bilelim - Sayfa bulunamadý.</title>
-            </Helmet>
-            <img style={img} src={Code404} alt="" />
-            <p style={p}>Aradýðýnýz sayfa/makaleyi bulamadýk. <Link style={a} to="/">Buradan anasayfa'ya dönerek,</Link> mevcut içeriklerimize göz atabilirsiniz.</p>
-        </section>
-    )
-}
+  return (
+    <section style={section}>
+      <Helmet>
+        <title>Bilelim - Sayfa bulunamadÄ±.</title>
+      </Helmet>
+      <img style={img} src={Code404} alt="" />
+      <p style={p}>
+        AradÄ±ÄŸÄ±nÄ±z sayfa/makaleyi bulamadÄ±k.{" "}
+        <Link style={a} to="/">
+          Buradan anasayfa'ya dÃ¶nerek,
+        </Link>{" "}
+        mevcut iÃ§eriklerimize gÃ¶z atabilirsiniz.
+      </p>
+    </section>
+  );
+};
 
 export default NotFound;

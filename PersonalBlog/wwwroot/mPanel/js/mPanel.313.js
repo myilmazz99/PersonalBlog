0dcc6bfffc2fe125ec25962eec91734edf721883
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-01-17 23:51:00 +0300

    added new panel ui libraries

---
+++ PersonalBlog/wwwroot/mPanel/js/mPanel.313.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmPanel=self.webpackChunkmPanel||[]).push([[313],{313:function(e,l,t){"use strict";t.r(l);var i=t(575),n=t.n(i);t(890),t(860),t(875),t(682),l.default=function(e="dist/tinymce"){n().init({base_url:e,selector:".editor",plugins:["paste","link","image","lists","media"],toolbar:"undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | image media",language:"tr_TR",language_url:e+"/langs/tr_TR.js"})}}}]);
+//# sourceMappingURL=mPanel.313.js.map
